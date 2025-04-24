0b220a011a4b885d87fbcc30a2463c04b103febd
omar <omaraziz.dev@proton.me>
2025-04-24 13:22:05 -0700

    txt

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,6 +5,13 @@ export default createRoute((context, _next) => {
   return context.render(
     <main class="uppercase space-y-8 w-full">
       <Form />
+      <div class="text-center text-pretty">
+        <p>
+          Copy an extension URL from Microsoft Marketplace and paste it here to download the
+          extension. Then drag the `.visx` and drop on the extensions view in Cursor / Windsurf /
+          VSCode.
+        </p>
+      </div>
     </main>,
   )
 })
